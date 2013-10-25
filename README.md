270153c640c33162a86a3c5f5ae6c046cb3a17bb
Russ Miles <russ@russmiles.com>
2013-10-25 09:21:09 +0200

    First version of the Koan, ready for adding the actual Koan code

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
-core-spring-distilled-koan-4
-============================
+# Core Spring Distilled: Koan 4
 
-Koan 4 from Core Spring Distilled
+This is a Koan from the Core Spring Distilled book from O'Reilly Media.
+
+To run the Koan, you can use the packaged `gradlew` scripts by invoking the following command:
+
+> \> ./gradlew -q test
+
---
+++ complete/src/test/groovy/com/simplicityitself/corespringdistilled/koans/Koan3.groovy
@@ -0,0 +1,24 @@
+package com.simplicityitself.corespringdistilled.koans
+
+import com.simplicityitself.corespringdistilled.koans.persistence.Repository
+import com.simplicityitself.corespringdistilled.koans.persistence.SampleRepository
+import org.springframework.beans.factory.support.BeanDefinitionBuilder
+import org.springframework.context.support.GenericApplicationContext
+import spock.lang.Specification
+
+class Koan3 extends Specification {
+    def "Add a Spring Bean Programmatically To An Application Context and Retrieve by the Spring Bean's Type"() {
+
+        given: "You have an Application Context and have registered a Bean with the Bean Builder"
+        GenericApplicationContext applicationContext = new GenericApplicationContext();
+        BeanDefinitionBuilder builder = BeanDefinitionBuilder.rootBeanDefinition(SampleRepository.class);
+
+        when: "Register the bean definition with the name 'repository' with the applicationContext"
+
+        applicationContext.registerBeanDefinition("repository", builder.getBeanDefinition());
+
+        then: "Bean retrieved from the ApplicationContext should not be null"
+        Repository beanRetrievedByType = applicationContext.getBean(Repository.class);
+        beanRetrievedByType != null;
+    }
+}  
